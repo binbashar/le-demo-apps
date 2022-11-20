d96a763589bcf46e0c1166852073409822a61553
Diego OJ <diego.ojeda@binbash.com.ar>
2022-11-19 21:57:43 -0300

    Make TF Bootstrap layer more customizable

--- argocd/plug-example/terraform-bootstrap/main.tf
+++ argocd/plug-example/terraform-bootstrap/main.tf
@@ -1,13 +1,15 @@
 #
-# TODO Bootstrap plug-init and plug-apps
-# TODO Verify how TF deals with changes outside this layer...
-#      We'd only want TF to boostrap this layer but after we want to go full GitOps
+# TODO Add placeholders for namespaces and project/env
 # TODO Can we merge both init & apps into a single app?
 #
+locals {
+  name             = "plug"
+  argocd_namespace = "argocd"
+}
 
 resource "helm_release" "init" {
   name       = "plug-init"
-  namespace  = "argocd"
+  namespace  = local.argocd_namespace
   repository = "https://binbashar.github.io/helm-charts/"
   chart      = "raw"
   version    = "0.1.0"
@@ -17,15 +19,24 @@ resource "helm_release" "init" {
     - apiVersion: argoproj.io/v1alpha1
       kind: Application
       metadata:
-        name: plug-init
+        name: ${local.name}-init
+        namespace: ${local.argocd_namespace}
       spec:
         destination:
-          namespace: argocd
+          namespace: ${local.argocd_namespace}
           server: https://kubernetes.default.svc
         source:
           path: argocd/plug-example/apps-init
           repoURL: https://github.com/binbashar/le-demo-apps
           targetRevision: argocd
+          helm:
+            parameters:
+              - name: global.name
+                value: ${local.name}
+              - name: global.namespace
+                value: ${local.name}
+              - name: secret.path
+                value: secrets-v1/devops-tf-infra/apps-env/${local.name}
         project: default
     EOF
   ]
@@ -33,7 +44,7 @@ resource "helm_release" "init" {
 
 resource "helm_release" "plug_root" {
   name       = "plug-root"
-  namespace  = "argocd"
+  namespace  = local.argocd_namespace
   repository = "https://binbashar.github.io/helm-charts/"
   chart      = "raw"
   version    = "0.1.0"
@@ -43,16 +54,25 @@ resource "helm_release" "plug_root" {
     - apiVersion: argoproj.io/v1alpha1
       kind: Application
       metadata:
-        name: plug-root
+        name: ${local.name}-root
+        namespace: ${local.argocd_namespace}
       spec:
         destination:
-          namespace: plug
+          namespace: ${local.argocd_namespace}
           server: https://kubernetes.default.svc
         source:
           path: argocd/plug-example/apps
           repoURL: https://github.com/binbashar/le-demo-apps
           targetRevision: argocd
-        project: plug
+          helm:
+            parameters:
+              - name: global.prefix
+                value: ${local.name}
+              - name: global.namespace
+                value: ${local.argocd_namespace}
+              - name: global.destination.namespace
+                value: ${local.name}
+        project: ${local.name}
     EOF
   ]
 }
